cf6ac0b710c4645ce202fc7dc976383d66b91a45
LucasAndrade <lucasandrade@LAPC.local>
2014-06-13 12:01:01 -0300

    criando pasta dentro do repositorio com teste de projeto para o eclipse aceitar jade com git

---
+++ projetoMultiAgentes/src/projetoMultiAgentes/AgenteOlaMundo.java
@@ -0,0 +1,24 @@
+package projetoMultiAgentes;
+import jade.core.Agent;
+
+
+
+public class AgenteOlaMundo extends Agent  {
+
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2501955863771943334L;
+
+	protected void setup(){
+		System.out.println("Ola mundo!");
+System.out.println("Meu nome");
+}
+	
+	private void metodo(){
+		
+	}
+	
+}
